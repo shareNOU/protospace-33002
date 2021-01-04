160fd8b66a2cdb77300f69046862939e4aa3acca
shareNOU <75669928+shareNOU@users.noreply.github.com>
2021-01-04 14:47:38 +0900

    適切なビューファイルを設置

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "prototypes#index"
-  resources :prototypes, only: [:new, :create]
+  resources :prototypes, only: [:new, :create, :show]
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
