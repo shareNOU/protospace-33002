c6a57d177369e6807ab5566efcebb492ddcda46e
shareNOU <75669928+shareNOU@users.noreply.github.com>
2021-01-04 18:45:42 +0900

    削除機能

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -34,6 +34,12 @@ class PrototypesController < ApplicationController
   end
 
   def destroy
+    @prototype = Prototype.find(params[:id])
+    if @prototype.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
 
   private
