dcdcc7baac70fbcce79561f219d1dd285ddf5b07
shareNOU <75669928+shareNOU@users.noreply.github.com>
2021-01-05 11:17:36 +0900

    ユーザーのアクセス範囲の制限

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :prototypes, only: [:new, :create, :show, :edit, :update, :destroy] do
     resources :comments, only: :create
   end
-  resources :user, only: :show
+  resources :users, only: :show
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
