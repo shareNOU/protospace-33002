2440dde7446d1a36263c35a504e6ba73e818d859
shareNOU <75669928+shareNOU@users.noreply.github.com>
2021-01-05 10:29:24 +0900

    コントローラーとアクション、ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :prototypes, only: [:new, :create, :show, :edit, :update, :destroy] do
     resources :comments, only: :create
   end
+  resources :user, only: :show
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
