9974778b44654a3547fff8c49b4e0978e82181d3
shareNOU <75669928+shareNOU@users.noreply.github.com>
2021-01-05 13:04:11 +0900

    修正、コメント同時消し

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root to: "prototypes#index"
   resources :prototypes, only: [:new, :create, :show, :edit, :update, :destroy] do
     resources :comments, only: :create
+    collection do
+      get 'search'
+    end
   end
   resources :users, only: :show
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
